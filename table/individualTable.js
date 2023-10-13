1fe1323f1114cb292691ff1b32863803e499a1bf
Bhuvanesh Dorle <22070838@ycce.in>
2023-10-13 10:19:36 +0530

    final edit

--- table/individualTable.js
+++ table/individualTable.js
@@ -19,7 +19,7 @@ async function fetchDataFromDb() {
     snapshot.docs.forEach(doc => {
         const docId = doc._delegate._key.path.segments[6]; // Retrieving the ID
         const userData = doc.data();
-        AddItemToTable(userData, docId);  // Make sure you're passing the doc.id here
+        AddItemToTable(userData, docId);  
     });
 }
 
@@ -37,8 +37,8 @@ function handleApply(event) {
 async function deleteFromFirebase(docId) {
     try {
         await firestore.collection('users').doc(docId).delete();
-        alert("Applied successfully!");
-        location.reload();  // reload the page to reflect the changes
+        alert("Applied successfully! We will contact you Shortly.");
+        location.reload();  
     } catch (error) {
         console.error("Error deleting document: ", error);
     }
